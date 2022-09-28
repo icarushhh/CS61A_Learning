3af7982f74416de4660189fb67ceb58388eee237
icarus <icarushhh@mail.ustc.edu.cn>
2022-09-28 20:12:08 +0800

    learn lec01

--- Lec01/01.py
+++ Lec01/01.py
@@ -1,3 +1,5 @@
+# run in a interpreter
+
 # Numeric expressions
 2022
 2000 + 22
@@ -11,10 +13,10 @@ abs(-2)
 
 # Objects
 # Note: Download from http://composingprograms.com/shakespeare.txt
-shakes = open("shakespeare.txt")
+shakes = open("/Users/icarus/Library/Mobile Documents/com~apple~CloudDocs/2022秋季学期/CS61A_Learning/Lec01/shakespeare.txt")
 text = shakes.read().split()
 print(len(text))
-text[:25]
+print(text[:25])
 text.count('the')
 text.count('thou')
 text.count('you')
@@ -22,16 +24,17 @@ text.count('forsooth')
 text.count(',')
 
 # Sets
+# something in a set only shows up once
 words = set(text)
-len(words)
+print(len(words))
 
 # Combinations 
 'draw'
-'draw'[0]
-{w[0] for w in words}
+print('draw'[0])
+print({w[0] for w in words})        # {} represent a set
 
 # Data
-'draw'[::-1]
-{w for w in words if w == w[::-1] and len(w)>4}
-{w for w in words if w[::-1] in words and len(w) == 4}
-{w for w in words if w[::-1] in words and len(w) > 6}
+print('draw'[::-1])        # reverse a word
+print({w for w in words if w == w[::-1] and len(w)>4})
+print({w for w in words if w[::-1] in words and len(w) == 4})
+print({w for w in words if w[::-1] in words and len(w) > 6})
